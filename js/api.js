0ea79e4c2065bd22a330ed384c9f488ee7ee2d1c
brudneyuk <brudney.ramosjr@sptech.school>
2026-05-21 21:15:48 -0300

    feat: api.js

--- js/api.js
+++ js/api.js
@@ -5,7 +5,7 @@
 
 (function () {
   // URL vazia = URL relativa. nginx faz proxy /api/ → Spring Boot :8080
-  const API_BASE_URL = "https://ceslc02jni.execute-api.us-east-1.amazonaws.com/";
+  const API_BASE_URL = "https://ldi2hf9mod.execute-api.us-east-1.amazonaws.com/";
 
   // ========== Utils ==========
   function getToken() {
